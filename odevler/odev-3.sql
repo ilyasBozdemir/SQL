07a41dea30a68e87c1f6a792ddc50e07993e2beb
İlyas Bozdemir <52322835+ilyasBozdemir@users.noreply.github.com>
2022-04-02 21:23:18 +0300

    Update odev-3.sql

--- odevler/odev-3.sql
+++ odevler/odev-3.sql
@@ -1,4 +1,4 @@
--- Odev 2
+-- Odev 3
 --country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
 SELECT country FROM country WHERE country LIKE 'A%a'
 --country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
